57ad33a8cf4ffe060ab0a1cae1b9ab3ff9f1d847
KasperVesteraa <kv19@student.aau.dk>
2023-03-06 10:54:36 +0100

    get_prices() reviewed

--- app/infrastructure/edsfetcher.py
+++ app/infrastructure/edsfetcher.py
@@ -1,28 +1,35 @@
 import requests
+from typing import List
 
-class EDSFetcher:
-    def __init__(self, endpoint: str, file_path: str):
+from .eletricity_prices import ElectricityPrices, PricePoint
+
+class EDSFetcher(ElectricityPrices):
+    def __init__(self, endpoint: str):
         self.endpoint = endpoint
-        self.file_path = file_path
 
-    def fetch_data(self) -> None:
+    def get_prices(self, start_time=None, end_time=None) -> List[PricePoint]: # start_time and end_time not used in MVP
         try:
             response = requests.get(url=self.endpoint)
         except Exception as e:
             print("Error fetching data: ", e)
 
+        # We are only interested in the records that are in the DK1 price area
         result =  response.json()
         records = result.get('records', [])
         records = [record for record in records if record['PriceArea'] == 'DK1']
         records = [{k: v for k, v in record.items() if k in ['HourDK', 'SpotPriceDKK']} for record in records]
 
-        try:
-            with open(self.file_path, 'w') as f:
-                f.write(str(records))
-        except Exception as e:
-            print("Error writing to file: ", e)
+        # For each record in records, create a PricePoint object and add it to the list
+        price_points = []
+        for record in records:
+            price_point = PricePoint()
+            price_point.time = record['HourDK']
+            price_point.price = record['SpotPriceDKK']
+            price_points.append(price_point)
+
+        return price_points
 
 if __name__ == "__main__":
     endpoint = 'https://api.energidataservice.dk/dataset/Elspotprices?limit=50'
-    file_path = '/home/dremacs/github/backend/app/infrastructure/data/spot_prices.json'
-    EDSFetcher(endpoint, file_path).fetch_data()
+    price_points = EDSFetcher(endpoint).get_prices()
+    print(price_points)
--- app/infrastructure/eletricity_prices.py
+++ app/infrastructure/eletricity_prices.py
@@ -4,9 +4,10 @@ from typing import List
 
 class PricePoint:
     def __init__(self) -> None:
-        pass
+        self.time: time
+        self.price: float
 
 class ElectricityPrices(ABC):
     @abstractmethod
-    def get_prices(self, start_date: time , end_date: time) -> List[PricePoint]:
+    def get_prices(self, start_time: time , end_time: time) -> List[PricePoint]:
         pass
