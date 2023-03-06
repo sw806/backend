3105f3b0e76a1a749fb6acf4a24e52e083f7b008
KasperVesteraa <kv19@student.aau.dk>
2023-03-06 10:58:17 +0100

    get_prices() reviewed

---
+++ app/infrastructure/test_edsfetcher.py
@@ -0,0 +1,11 @@
+import os.path
+from .edsfetcher import EDSFetcher
+import pytest
+
+class TestEDSFetcher:
+    def test_fetch_data_bad_endpoint_causes_exception(self):
+        # Arrange
+        endpoint = 'https://invalid-endpoint'
+        # Act/Assert
+        with pytest.raises(Exception):
+            EDSFetcher(endpoint).get_prices()
