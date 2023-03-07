7676e3b5cfdb597bb49599aa3af2b2aed9412887
KasperVesteraa <kv19@student.aau.dk>
2023-03-07 11:05:04 +0100

    fix(Scheduling): Updated calculate_optimal_time() and PricePoint with datetime type

--- app/infrastructure/optimal_time_calculator.py
+++ app/infrastructure/optimal_time_calculator.py
@@ -9,7 +9,8 @@ class OptimalTimeCalculator:
         pass
 
     def calculate_optimal_time(self, price_points: List[PricePoint], energy: Optional[int], power: Optional[int],
-                               duration: Optional[datetime.time]) -> datetime.time:
+                               duration: Optional[datetime.time]) -> datetime:
+        # Har lige et par ting vi kan tale om ang. energy, power og duration
         if duration is None:
             if energy is None or power is None:
                 raise ValueError("Both energy and power must be provided if duration is not specified")
@@ -17,7 +18,6 @@ class OptimalTimeCalculator:
             duration = datetime.timedelta(hours=duration_hours)
 
         no_of_intervals = math.ceil(duration.total_seconds() / 3600)
-        # Find the interval with the lowest price based on the number of intervals and the PricePoint list
         lowestSum = float('inf')
         optimal_start_time = None
         for i in range(len(price_points) - (no_of_intervals - 1)):
@@ -31,10 +31,9 @@ class OptimalTimeCalculator:
 
         return optimal_start_time
 
-
 if __name__ == "__main__":
     price_points = EDSFetcher('https://api.energidataservice.dk/dataset/Elspotprices?limit=50').get_prices()
     optimal_time_calculator = OptimalTimeCalculator()
-    optimal_time = optimal_time_calculator.calculate_optimal_time(price_points, 1, 1, None)
+    optimal_time = optimal_time_calculator.calculate_optimal_time(price_points, 3, 1, None)
     print("Optimal start time: ", optimal_time)
 
