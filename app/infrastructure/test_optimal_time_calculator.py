46666996369c7bb5d46e89dfa8b6050841439276
KasperVesteraa <kv19@student.aau.dk>
2023-03-10 13:11:05 +0100

    feat(Schedule): New calculate_optimal_time function begun

--- app/infrastructure/test_optimal_time_calculator.py
+++ app/infrastructure/test_optimal_time_calculator.py
@@ -14,7 +14,6 @@ class TestOptimalTimeCalculator:
             PricePoint(datetime(2021, 1, 1, 17), 10.0),
             PricePoint(datetime(2021, 1, 1, 18), 10.0),
             PricePoint(expected, 5.0),
-            PricePoint(datetime(2021, 1, 1, 19), 10.0),
             PricePoint(datetime(2021, 1, 1, 20), 10.0),
             PricePoint(datetime(2021, 1, 1, 21), 10.0),
             PricePoint(datetime(2021, 1, 1, 22), 10.0)
@@ -28,13 +27,13 @@ class TestOptimalTimeCalculator:
 
     def test_two_hour_span(self):
         # Arrange
-        expected: datetime = datetime(2021, 1, 1, 18)
+        expected: datetime = datetime(2021, 1, 1, 17)
         price_points: List[PricePoint] = [
             PricePoint(datetime(2021, 1, 1, 15), 10.0),
             PricePoint(datetime(2021, 1, 1, 16), 10.0),
             PricePoint(datetime(2021, 1, 1, 17), 10.0),
-            PricePoint(datetime(2021, 1, 1, 18), 10.0),
             PricePoint(expected, 5.0),
+            PricePoint(datetime(2021, 1, 1, 19), 10.0),
             PricePoint(datetime(2021, 1, 1, 20), 10.0),
             PricePoint(datetime(2021, 1, 1, 21), 10.0),
             PricePoint(datetime(2021, 1, 1, 22), 10.0),
@@ -67,27 +66,6 @@ class TestOptimalTimeCalculator:
 
         # Assert
         assert optimal_time == int(expected.timestamp())
-
-        def test_one_hour_span_early_edge_case(self):
-            # Arrange
-            expected: datetime = datetime(2021, 1, 1, 15)
-            price_points: List[PricePoint] = [
-                PricePoint(expected, 5.0),
-                PricePoint(datetime(2021, 1, 1, 16), 10.0),
-                PricePoint(datetime(2021, 1, 1, 17), 10.0),
-                PricePoint(datetime(2021, 1, 1, 18), 10.0),
-                PricePoint(datetime(2021, 1, 1, 19), 10.0),
-                PricePoint(datetime(2021, 1, 1, 20), 10.0),
-                PricePoint(datetime(2021, 1, 1, 21), 10.0),
-                PricePoint(datetime(2021, 1, 1, 22), 10.0),
-                PricePoint(datetime(2021, 1, 1, 23), 10.0)
-            ]
-
-            # Act
-            optimal_time = OptimalTimeCalculator().calculate_optimal_time(price_points, 1, 3600)
-
-            # Assert
-            assert optimal_time == int(expected.timestamp())
 
     def test_one_hour_span_late_edge_case(self):
         # Arrange
