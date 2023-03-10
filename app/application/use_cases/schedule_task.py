7f80dd3ce7bd5826ef0aa88fa1937c339b3236f5
Andreas <akbr18@student.aau.dk>
2023-03-10 11:29:13 +0100

    fix(scheduler): removed main run and comments

--- app/application/use_cases/schedule_task.py
+++ app/application/use_cases/schedule_task.py
@@ -28,12 +28,4 @@ class ScheduleTaskUseCase:
         optimal_time = OptimalTimeCalculator()\
             .calculate_optimal_time(price_points, request.power, request.duration)
         print("Optimal start time: ", optimal_time)
-        #optimal_time_unix = int(optimal_time.timestamp())
-        #return ScheduleTaskResponse(optimal_time_unix)
         return ScheduleTaskResponse(optimal_time)
-
-
-if __name__ == "__main__":
-    request = ScheduleTaskRequest(3, 1, None)
-    response = ScheduleTaskUseCase().do(request)
-    print(response.start_date)
