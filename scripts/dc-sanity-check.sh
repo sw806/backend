bcbfb74ce5671be2b1c4404755349ccd3ba0fe0a
Andreas <akbr18@student.aau.dk>
2023-03-13 11:04:25 +0100

    fix(sanity): Fixed exit variable

--- scripts/dc-sanity-check.sh
+++ scripts/dc-sanity-check.sh
@@ -8,8 +8,8 @@ EXIT=0
 docker compose up -d
 sleep 1s
 curl --fail --retry 10 http://0.0.0.0:8000/api/v1/status \
-    && echo "" && echo "${GREEN}==============================Sanity check PASSED!==============================${NC}" \
-    || echo "${RED}==============================Sanity check FAILED!==============================${NC}" && EXIT=1
+    && (echo "" && echo "${GREEN}==============================Sanity check PASSED!==============================${NC}" && EXIT=0) \
+    || (echo "${RED}==============================Sanity check FAILED!==============================${NC}" && EXIT=1)
 echo ""
 docker compose down
 exit ${EXIT}
