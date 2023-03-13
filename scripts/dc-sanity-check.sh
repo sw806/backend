cf97207ac85591db381d7a3bbdf6f7e11a8cc937
Andreas <akbr18@student.aau.dk>
2023-03-13 10:37:34 +0100

    test (Sanity): Removed exit

--- scripts/dc-sanity-check.sh
+++ scripts/dc-sanity-check.sh
@@ -3,11 +3,13 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+EXIT=0
 
 docker compose up -d
 sleep 1s
 curl --fail --retry 10 http://0.0.0.0:8010/api/v1/status \
     && echo "" && echo "${GREEN}==============================Sanity check PASSED!==============================${NC}" \
-    || echo "${RED}==============================Sanity check FAILED!==============================${NC}" && exit -1
+    || echo "${RED}==============================Sanity check FAILED!==============================${NC}" && EXIT=1
 echo ""
 docker compose down
+exit ${EXIT}
