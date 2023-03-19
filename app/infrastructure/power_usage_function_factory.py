3bfecdc47cbdbd15886e1c45dc03746e2e377c9b
Andreas <akbr18@student.aau.dk>
2023-03-19 15:36:26 +0100

    feat(PowerFunctionFactory): Created factory methods for the function

---
+++ app/infrastructure/power_usage_function_factory.py
@@ -0,0 +1,22 @@
+from datetime import timedelta
+from typing import List, Tuple
+from infrastructure.power_usage_function import PowerUsageFunction
+
+
+class PowerUsageFunctionFactory():
+    def __init__(self) -> None:
+        pass
+
+    def create_constant_consumption(
+        self, duration: timedelta, power: float
+    ) -> PowerUsageFunction:
+        return PowerUsageFunction(
+            [(duration, power)], extend_by=timedelta()
+        )
+    
+    def create_variable_consumption(
+        self, power_points: List[Tuple[timedelta, float]]
+    ) -> PowerUsageFunction:
+        return PowerUsageFunction(
+            power_points, extend_by=timedelta()
+        )
