ac1d9212cdf5086f77ea759de7a77939bba449dc
Andreas <akbr18@student.aau.dk>
2023-03-28 14:25:24 +0200

    fix(Schedules router): Changed function names to have versions

--- app/presentation/schedules_router.py
+++ app/presentation/schedules_router.py
@@ -7,14 +7,14 @@ schedules_router_v1 = APIRouter(prefix="/api/v1")
 schedules_router_v2 = APIRouter(prefix="/api/v2")
 
 @schedules_router_v1.post("/schedules")
-async def schedule(request: ScheduleTaskRequest) -> Any:
+async def schedule_v1(request: ScheduleTaskRequest) -> Any:
     try:
         return User().schedule_task(request)
     except Exception as e:
         return "Error: " + str(e)
 
 @schedules_router_v2.post("/schedules")
-async def schedule(request: ScheduleTasksRequest) -> Any:
+async def schedule_v2(request: ScheduleTasksRequest) -> Any:
     try:
         print(request)
         return User().schedule_tasks(request)
