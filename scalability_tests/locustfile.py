131d75588023333e2a11fedc5a479b03105bd3a3
KasperVesteraa <kv19@student.aau.dk>
2023-04-17 13:28:54 +0200

    feat(Scalability-tests): Autogenerated json ver 1.0

--- scalability_tests/locustfile.py
+++ scalability_tests/locustfile.py
@@ -1,11 +1,11 @@
-import json
-
 from locust import HttpUser, task, between
+from generate_json import generate_json
+import json
 
 class ScalabilityTester(HttpUser):
     wait_time = between(1,5)
 
-    @task(0)
+    @task(1)
     def v1_schedules(self):
         param = {"duration": 100, "power": 1}
         response = self.client.post("/api/v1/schedules", json=param)
@@ -13,13 +13,12 @@ class ScalabilityTester(HttpUser):
         if response.status_code != 200:
             print(f"Failed request with status code: {response.status_code}, content: {response.content}")
 
-    @task(1)
+    @task(0)
     def v2_schedules(self):
-        # param = json.loads("/home/dremacs/Dropbox/2023/sem8/P8/backend/scalability_tests/v2_req.json")
+        # with open("/home/dremacs/Dropbox/2023/sem8/P8/backend/scalability_tests/v2_req.json") as f:
+        #     param = json.load(f)
 
-        with open("/home/dremacs/Dropbox/2023/sem8/P8/backend/scalability_tests/v2_req.json") as f:
-            param = json.load(f)
-
+        param = json.loads(generate_json())
         response = self.client.post("/api/v2/schedules", json=param)
 
         if response.status_code != 200:
